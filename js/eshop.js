a6223e63b327f636873309d692ad7d93634b9849
Jérémy <elvynia@gmail.com>
2018-12-07 15:06:57 +0100

    Ajout du code JavaScript pour charger le fichier JSON et ajouter les options à la liste déroulante.

--- js/eshop.js
+++ js/eshop.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
     // Le code à l'intérieur de cette fonction est interprêté seulement lorsque tout le document HTML est chargé.
+    $.ajax({
+        url: "choco_types.json"
+    }).done(function(chocoTypes) {
+        var chocoSelect = $("#choco");
+        for (let choco of chocoTypes) {
+            let newOption = $('<option value="' + choco.value + '">' + choco.label + "</option>");
+            chocoSelect.append(newOption);
+        }
+    });
 });
